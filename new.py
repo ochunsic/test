01cb2de232f2efd0857fa699a72d6f13ad5a4f3b
ochunsic <38836428+ochunsic@users.noreply.github.com>
2018-06-06 21:25:07 +0900

    Create new.py

---
+++ new.py
@@ -0,0 +1,11 @@
+#for문 테스트
+squares =[]
+for value  in range(100):
+    square = value**2
+    squares.append(square)
+
+print(squares)
+
+print(min(squares))
+print(max(squares))
+print(sum(squares))
